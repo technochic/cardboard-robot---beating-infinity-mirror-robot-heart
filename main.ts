b368bbb6a54605034ad8ff38c5c899a79ac4fd68
Natasha <technochic@users.noreply.github.com>
2021-10-12 21:24:44 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 led.enable(false)
-let strip = neopixel.create(DigitalPin.P1, 17, NeoPixelMode.RGB)
+let strip = neopixel.create(DigitalPin.P1, 18, NeoPixelMode.RGB)
 basic.forever(function () {
     strip.showColor(neopixel.rgb(25, 0, 5))
     strip.show()
-    for (let index = 0; index <= 17; index++) {
+    for (let index = 0; index <= 18; index++) {
         strip.rotate(1)
         strip.setPixelColor(index, neopixel.colors(NeoPixelColors.White))
         strip.setPixelColor(index - 1, neopixel.rgb(255, 0, 50))
